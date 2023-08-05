22dca1e23c6bb4d3585dd296f1b3a5a330c96988
aswnss_ubuntu <aswinlalsct@gmail.com>
2023-08-05 15:31:27 +0530

    t and c base done

--- src/Pages/Landingpage.jsx
+++ src/Pages/Landingpage.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import TandCmodal from '../components/TandCmodal';
 function Landingpage() {
+  const [showModal, setShowModal] = useState(false); // State to control modal visibility
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className='flex h-screen items-center'>
-        <h1>Something</h1>
+    <div className="flex h-screen items-center w-screen justify-center">
+      <div className='flex justify-center items-center flex-col gap-2 w-1/2'>
+        <h1 className='text-2xl font-bold'>Start Test</h1>
+        <div className="info flex gap-2">
+          <h3 className='bg-gray-200 p-2 rounded'>{30} Questions</h3>
+          <h3 className='bg-gray-200 p-2 rounded'>{'10:00'} mins </h3>
+        </div>
+        <button
+          className='bg-green-400 hover:bg-green-200 p-3 rounded w-1/2'
+          onClick={openModal}
+        >
+          Start
+        </button>
+      </div>
+      {showModal && (
+        <TandCmodal onClose={closeModal} />
+      )}
     </div>
-  )
+  );
 }
 
-export default Landingpage
+export default Landingpage;
---
+++ src/components/TandCmodal.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+const TandCmodal = ({ onClose }) => {
+  const [cameraPermission, setCameraPermission] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  const handleCameraPermissionChange = () => {
+    if (!cameraPermission) {
+      // Code to request camera permission goes here
+      // You can use browser APIs like navigator.mediaDevices.getUserMedia()
+      // to request camera access
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(() => {
+          console.log('Camera permission granted');
+          setCameraPermission(true);
+        })
+        .catch((error) => {
+          console.log('Camera permission denied:', error);
+          setCameraPermission(false);
+        });
+    } else {
+      setCameraPermission(false);
+    }
+  };
+
+  const handleAcceptTerms = () => {
+    if (cameraPermission && termsAccepted) {
+      // Perform any necessary actions when terms are accepted and camera permission is granted
+      console.log('Terms accepted and camera permission granted');
+    } else if (!cameraPermission) {
+      // Display a message or take appropriate action if camera permission is not granted
+      console.log('Please grant camera permission to accept the terms');
+    } else if (!termsAccepted) {
+      // Display a message or take appropriate action if terms are not accepted
+      console.log('Please accept the terms and conditions');
+    }
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="absolute bg-white rounded-lg shadow-lg p-6">
+        <h2 className="text-xl font-bold mb-4">Terms and Conditions</h2>
+        <p className="mb-4">
+          These are the terms and conditions for the online aptitude test. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam faucibus, justo eu malesuada tincidunt, felis libero varius neque, in volutpat tortor nulla sit amet libero.
+        </p>
+        <div className="flex items-center mb-4">
+          <input
+            type="checkbox"
+            id="cameraPermissionCheckbox"
+            className="mr-2"
+            checked={cameraPermission}
+            onChange={handleCameraPermissionChange}
+          />
+          <label htmlFor="cameraPermissionCheckbox">Grant camera permission</label>
+        </div>
+        {cameraPermission && (
+          <div className="flex items-center mb-4">
+            <input
+              type="checkbox"
+              id="termsCheckbox"
+              className="mr-2"
+              checked={termsAccepted}
+              onChange={() => setTermsAccepted(!termsAccepted)}
+            />
+            <label htmlFor="termsCheckbox">I agree to the terms and conditions</label>
+          </div>
+        )}
+        <div className="flex items-center">
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+              (!cameraPermission || !termsAccepted) ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            onClick={handleAcceptTerms}
+            disabled={!cameraPermission || !termsAccepted}
+          >
+            Accept
+          </button>
+          <button className="ml-2 text-gray-500" onClick={onClose}>
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TandCmodal;
