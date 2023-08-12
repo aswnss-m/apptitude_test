18bf42284526cd1bf718bf48bd0be511894a765e
aswnss <aswinlalsct@gmail.com>
2023-08-12 21:52:22 +0530

    changed DOM error

--- src/Pages/Fail.jsx
+++ src/Pages/Fail.jsx
@@ -16,8 +16,9 @@ function Fail() {
       <h1 className='text-5xl text-tertiary font-bold'>Exam Failed</h1>
       <p>We will not contact you.</p>
       <p style={{ display: 'flex', alignItems: 'center' }}>
-        You will be redirected in...<span style={{ marginLeft: '1px' }}><TimerApp timerDuration={5} helper={changeTimer}/></span>
+        You will be redirected in..
       </p>
+      <TimerApp timerDuration={5} helper={changeTimer}/>
     </div>
   );
 }
